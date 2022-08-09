dc48905dbc918c6a689ea6c6797cb79f19da15ef
alqinsidev <padlanalqinsi@live.com>
2022-08-10 00:49:01 +0700

    add error port

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"TODO-DIGITALENT/config"
 	"TODO-DIGITALENT/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := config.EnvPort()
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	config.ConnectDB()
 	server := gin.Default()
 	routes.Route(server)
-	server.Run(":" + config.EnvPort()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	server.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
