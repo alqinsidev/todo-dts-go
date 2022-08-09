a667b3c0fdc466b8844e42a82ad4fb56a901f696
alqinsidev <padlanalqinsi@live.com>
2022-08-10 06:27:56 +0700

    add html to entry point

--- routes/Route.go
+++ routes/Route.go
@@ -8,11 +8,6 @@ import (
 )
 
 func Route(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "world",
-		})
-	})
 
 	r.POST("/tasks", controller.CreateTask())
 	r.GET("/tasks", controller.GetTasks())
@@ -20,7 +15,7 @@ func Route(r *gin.Engine) {
 	r.PUT("/tasks", controller.EditTask())
 
 	r.LoadHTMLGlob("templates/*.html")
-	r.GET("/web", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 }
